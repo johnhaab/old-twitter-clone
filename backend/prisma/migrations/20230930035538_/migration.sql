8146b7324d4cd01348a33ee04e5ab1ccff948d93
johnhaab <wearedevelopersbiz@gmail.com>
2023-09-30 00:36:58 -0400

    Add backend w/ basic auth system & password hashing & info validation

---
+++ backend/prisma/migrations/20230930035538_/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tweet" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Tweet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Like" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "tweetId" INTEGER NOT NULL,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Like_userId_tweetId_key" ON "Like"("userId", "tweetId");
+
+-- AddForeignKey
+ALTER TABLE "Tweet" ADD CONSTRAINT "Tweet_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_tweetId_fkey" FOREIGN KEY ("tweetId") REFERENCES "Tweet"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ backend/server.js
@@ -0,0 +1,144 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const express = require("express");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const PORT = 3001;
+
+const secretKey = process.env.SECRET_KEY;
+
+app.post("/signup", async (req, res) => {
+  const { name, username, email, password1, password2 } = req.body;
+
+  // First, check if the username is already taken
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (user) {
+    return res.status(404).json({ error: "Username taken, try another." });
+  }
+
+  // Second, check if the email is already taken
+  const userEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userEmail) {
+    return res.status(404).json({ error: "Email taken, try logging in." });
+  }
+
+  // Third, check if the the users passwords are correct, then hash the password and create the account with the hashedPassword.
+  if (password1 !== password2) {
+    return res.status(403).json({ error: "Passwords do not match." });
+  }
+
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(password1, salt, async function (err, hash) {
+      if (err) {
+        return res.status(500).json({ error: "Error hashing password" });
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          username,
+          password: hash, // Use the hashed password here
+        },
+      });
+
+      res.json(user);
+    });
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Find the user by username using Prisma
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+
+    //   // Second, compare the provided password with the stored hashed password
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (err) {
+        return res.status(500).json({ error: "Error comparing passwords" });
+      }
+
+      if (result) {
+        // Passwords match, user is authenticated
+        // Create a JWT token with a 24-hour expiration time
+        const expiration = 24 * 60 * 60; // 24 hours in seconds
+        const token = jwt.sign(
+          { userId: user.id, username: user.username },
+          secretKey,
+          {
+            expiresIn: expiration,
+          }
+        );
+
+        // Send the JWT as a cookie
+        res.cookie("authToken", token, {
+          maxAge: expiration * 1000, // Convert seconds to milliseconds
+          httpOnly: true, // Cookie is accessible only via HTTP/HTTPS, not JavaScript
+        });
+
+        res.json({ message: "Authentication successful" });
+      } else {
+        // Passwords don't match, authentication failed
+        res.status(403).json({ error: "Authentication failed" });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/protected-resource", (req, res) => {
+  // Verify the JWT from the cookie
+  const token = req.cookies.authToken;
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // JWT is valid, you can access the decoded data (e.g., userId, username)
+    const { userId, username } = decoded;
+
+    res.json({
+      message: `Welcome, ${username}! This is a protected resource for user ${userId}.`,
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ready at http://localhost:${PORT}`);
+});
